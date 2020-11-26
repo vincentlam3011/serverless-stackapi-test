2b7b6c9003eb27efbfffcbc4e9fb3e0ada4e1220
Hao Te Lam <haotlam@kms.com.vn>
2020-11-26 15:10:44 +0700

    Secure APIs

--- handlers/deleteItem.js
+++ handlers/deleteItem.js
@@ -5,7 +5,7 @@ export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     Key: {
-      userId: "Hao",
+      userId: event.requestContext.identity.cognitoIdentityId,
       noteId: event.pathParameters.id,
     },
   };
